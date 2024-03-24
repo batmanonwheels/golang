30b8b4c8dcedf5ffff1cc830739c2da0f00c19e3
Devin Garcia <devingarcia5423@gmail.com>
2024-03-24 15:18:49 -0400

    install tailwind cli

---
+++ gowiki/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./templates/*.html'], // This is where your HTML templates / JSX files are located
+	theme: {},
+	plugins: [],
+};
